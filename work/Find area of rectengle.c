0caaa17eefdaec8f6cf42086b86055c3d1b8b013
NishaRaiyani007 <nishuraiyani7@gmail.com>
2023-07-28 14:44:17 +0530

    123

---
+++ work/Find area of rectengle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int lenght, width, area;
+	printf("Enter lenght");
+	scanf("%d", &lenght);
+	printf("Enter width");
+	scanf("%d" , &width);
+	area=lenght*width;
+	printf("area is:%d,area");
+	return 0 ;
+}
