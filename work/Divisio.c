922e22b5237033b783fa7772925c8179a711bd53
NishaRaiyani007 <nishuraiyani7@gmail.com>
2023-08-08 12:39:27 +0530

    123

---
+++ work/Divisio.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int No1, No2, Div;
+	printf("Enter Your No1:");
+	scanf("%d", &No1);
+	printf("Enter Your No2:");
+	scanf("%d", &No2);
+	Div= No1/No2;
+	printf("%d/%d/%d=%d", No1,No2); 
+	return 0;
+} 
