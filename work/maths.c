622e13aeb87600b69ffe341828c0403a2336080b
NishaRaiyani007 <nishuraiyani7@gmail.com>
2023-08-10 11:09:51 +0530

    123

---
+++ work/maths.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int main()
+{
+//   int lenght , width , area ;
+//   printf("Enter Your Lenght:");
+//   scanf("%d", &lenght);
+//   printf("Enter Your width:");
+//   scanf("%d", &width);
+//   area = lenght * width ;
+//   printf("Your Area:%d", area);
+
+//     int base, hight, area ;
+//	 printf("Enter Your base:");
+//	 scanf("%d", &base);
+//	 printf("Enter Your hight:");
+//	 scanf("%d", &hight);
+//	 area = base * hight / 2 ;
+//	 printf("Erea Is:%d", area); 
+
+//     float radius, area, pi=3.14 ;
+//     printf("Enter Your radius:");
+//     scanf("%f", &radius);
+//     
+//     area = pi * radius * radius;
+//	 
+//	 printf("Area Is:%f", area); 
+
+//    int side, area ;
+//    printf("Enter Your Side:");
+//    scanf("%d", &side);
+//    
+//    area = side * side ;  
+//    
+//    printf("Your Side Area Is:%d", area);
+
+    
+    return 0 ;	
+}    
+
+
+
+   
+
+
+
+
+
+
+  
+    
+
+
+
+
+
+
