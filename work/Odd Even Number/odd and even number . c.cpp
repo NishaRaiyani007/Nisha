ff1775d313553b8bdf329c95ea5eb585efa4980c
NishaRaiyani007 <nishuraiyani7@gmail.com>
2023-08-12 11:11:24 +0530

    123

---
+++ work/Odd Even Number/odd and even number . c.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+	int i, num, even=0, odd=0 ;
+	for(i=1; i=10; i++)
+	{
+		printf("\n Enter the values:");
+		scanf("%d", &num);
+		if(num%2==0)
+		{
+			printf("/Even\n");
+			even+=num;
+		} else{
+			printf("\nodd\n");
+			odd+=num;
+		}
+	 } 
+}   
+
