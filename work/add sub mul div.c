2b21974febcb575d1c10f6dc19c05a900bccbab2
NishaRaiyani007 <nishuraiyani7@gmail.com>
2023-08-14 11:01:02 +0530

    123

---
+++ work/add sub mul div.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+
+        int a=200, b=100;
+		printf("The Addition Of A and B Is %d", a+b);
+		printf("\nThe Subtraction Of A and B Is %d", a-b);
+		printf("\nThe Multiplication Of A and B Is %d", a*b);
+		printf("\nThe Division Of A and B Is %d", a/b);
+		return 0 ;
+}
+		
