24bb1830c80787ea0114df867ead60c10fd30ee2
NishaRaiyani007 <nishuraiyani7@gmail.com>
2023-08-15 11:11:41 +0530

    123

---
+++ work/2.c
@@ -0,0 +1,13 @@
+ #include<stdio.h>
+ int main()
+ {
+ 
+ int a,b;
+       printf("Enter The Value is a:");
+       scanf("%d", &a);
+       printf("Enter The Value is b:");
+       scanf("%d", &b);
+       printf("The Addition is %d", a+b);
+	   return 0 ; 
+   }
+
