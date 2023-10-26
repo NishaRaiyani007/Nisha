89189cf669eb9dcc913d76db39a62adfd467b913
NishaRaiyani007 <nishuraiyani7@gmail.com>
2023-10-26 19:55:10 +0530

    123

---
+++ work/Two Value of mul add sub.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int a=10, b=20;
+	cout<<"The Addition of A and B is:"<<a+b<<endl;
+	cout<<"The Subtrection of A and B is:"<<a-b<<endl;
+	cout<<"The Multiplication of A and B is:"<<a*b<<endl;
+}
+
