f8cd19b68af63722fcf992a9a458b31961f2890e
NishaRaiyani007 <nishuraiyani7@gmail.com>
2023-11-22 10:24:15 +0530

    123

---
+++ work/sqrt sin cos.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+main()
+{
+	cout<<pow(2,5);
+	cout<<"\n"<<sqrt(64);
+	cout<<"\n"<<sin(78);
+	cout<<"\n"<<cos(65);
+	cout<<"\n"<<tan(54);
+	cout<<"\n"<<log(30);
+	cout<<"\n"<<ceil(78.67);
+}
