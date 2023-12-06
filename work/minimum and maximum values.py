a38295304cec9b4138f7a7d985f0df11d5674bb4
NishaRaiyani007 <nishuraiyani7@gmail.com>
2023-12-06 12:28:50 +0530

    123

---
+++ work/minimum and maximum values.py
@@ -0,0 +1,14 @@
+a=int(input("Enter A : "))
+b=int(input("Enter B : "))
+c=int(input("Enter c : "))
+
+if a>b:
+    if a>c:
+        print("A is Greater")
+    else:
+        print("C is Greater")
+else:
+     if b>c:    
+        print("B is Greater")
+     else:
+         print("C is Greater")
