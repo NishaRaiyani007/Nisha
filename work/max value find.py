55662c873c01b57e6a57181833330d63341d9244
NishaRaiyani007 <nishuraiyani7@gmail.com>
2023-12-12 12:25:30 +0530

    123

---
+++ work/max value find.py
@@ -0,0 +1,14 @@
+def maxof3(a,b,c):
+    if a>b:
+        if a>c:
+            print("A is max")
+        else:
+            print("B is Max")
+    elif b>c:
+        print("B is max")
+    else:
+        print("C is Max")
+maxof3(100,200,400)
+
+
+
