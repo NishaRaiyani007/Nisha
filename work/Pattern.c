0435f2c04afbf97c4f8421183b5ab2985b2c916f
NishaRaiyani007 <nishuraiyani7@gmail.com>
2023-12-15 14:01:28 +0530

    123

---
+++ work/Pattern.c
@@ -0,0 +1,188 @@
+//#include<stdio.h>
+//main()
+//{
+//	int row,col;
+//	for(row=1; row<=5; row++)
+//	{
+//		for(col=1; col<=5; col++)
+//		{
+//			printf("1");
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+
+//#include<stdio.h>
+//main()
+//{
+//	int row,col;
+//	for(row=1; row<=5; row++)
+//	{
+//		for(col=1; col<=row; col++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+
+//#include<stdio.h>
+//main()
+//{
+//	int row,col;
+//	for(row=1; row<=5; row++)
+//	{
+//		for(col=1; col<=row; col++)
+//		{
+//			printf("%d",row);
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+
+//#include<stdio.h>
+//main()
+//{
+//	int row, col;
+//	for(row=1; row<=5; row++)
+//	{
+//		for(col=1; col<=row; col++)
+//		{
+//			printf("%d",col);
+//		}
+//		printf("\n");
+//	}
+//}
+
+//#include<stdio.h>
+//main()
+//{
+//	int row, col;
+//	int n=1;
+//	for(row=1; row<=5; row++)
+//	{
+//		for(col=1; col<=row; col++)
+//		{
+//			printf("%d",n);
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int row, col;
+//	int n=65;
+//	for(row=1; row<=5; row++)
+//	{
+//		for(col=1; col<=row; col++)
+//		{
+//			printf("%c",n);
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+
+//#include<stdio.h>
+//main()
+//{
+//	int row, col;
+//	for(row=5; row>=1; row--)
+//	{
+//		for(col=1; col<=row; col++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+
+
+//#include<stdio.h>
+//main()
+//{
+//	int row,col;
+//	for(row=1; row<=5; row++)
+//	{
+//		for(col=1; col<=row; col++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+//#include<stdio.h>
+//main()
+//{
+//	int row,col;
+//	for(row=1; row<=5; row++)
+//	{
+//		for(col=1; col<=row; col++)
+//		{
+//			printf("%d",row);
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+
+#include<stdio.h>
+main()
+{
+	int row, col;
+	for(row=1; row<=5; row++)
+	{
+		for(col=1; col<=row; col++)
+		{
+			printf("%d",col);
+		}
+		printf("\n");
+	}
+}
+
+
+
+//#include<stdio.h>
+//main()
+//{
+//	int i,j;
+//	
+//	for(i=1; i<=10; i++)
+//	{
+//		for(j=1; j<=10-i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for(j=1; j<=i; j++)
+//		{
+//			printf(" *");
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+
+
+
+
+
+
+
+
+
